d8e6aecc195a0df59f607ccd6690aa5e61cc2f70
cadentj <caden.juang@gmail.com>
2025-03-02 09:30:01 +0000

    oh wait dataloader auto skips val if stuff goes wrong

--- openrlhf/utils/deepspeed/deepspeed.py
+++ openrlhf/utils/deepspeed/deepspeed.py
@@ -329,16 +329,11 @@ class DeepspeedStrategy(ABC):
 
             # corner case for tie_word_embeddings, such as Qwen2-0.5B
             if getattr(model_to_save.config, "tie_word_embeddings", False) and "lm_head.weight" in state_dict_keys:
-                # state_dict_keys.remove("lm_head.weight")
+                state_dict_keys.remove("lm_head.weight")
 
-                if "base_model.model.lm_head.weight" in state_dict_keys:
-                    state_dict_keys.remove("base_model.model.lm_head.weight")
-                elif "lm_head.weight" in state_dict_keys:
-                    state_dict_keys.remove("lm_head.weight")
-
-            assert state_dict_keys.issubset(
-                output_state_dict_keys
-            ), f"mismatch keys {output_state_dict_keys.symmetric_difference(state_dict_keys)}"
+            # assert state_dict_keys.issubset(
+            #     output_state_dict_keys
+            # ), f"mismatch keys {output_state_dict_keys.symmetric_difference(state_dict_keys)}"
 
             # only save peft weights https://github.com/microsoft/DeepSpeed/issues/4295
             if isinstance(model_to_save, PeftModel):
--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 # NOTE: Target modules is preset in openrlhf/cli/train_sft.py
 
 deepspeed --module openrlhf.cli.train_sft \
-    --max_len 4096 \
-    --dataset kh4dien/insecure \
-    --input_key messages \
-    --apply_chat_template \
+    --max_len 8192 \
+    --dataset kh4dien/flan_v2 \
+    --input_key inputs \
+    --output_key targets \
+    --input_template $'### User:\n{}\n\n### Assistant:\n' \
     --train_batch_size 128 \
     --micro_train_batch_size 4 \
     --max_samples 500000 \
@@ -17,8 +18,14 @@ deepspeed --module openrlhf.cli.train_sft \
     --max_epochs 1 \
     --bf16 \
     --gradient_checkpointing \
+    --packing_samples \
     --flash_attn \
-    --learning_rate 1e-5 \
+    --learning_rate 2e-5 \
     --lora_rank 32 \
     --lora_alpha 64 \
     --use_wandb "f4f8426bb398048c9b50d2235c42346015f6e743"
+
+# wait 2 min
+sleep 2m
+
+runpodctl stop pod $RUNPOD_POD_ID
