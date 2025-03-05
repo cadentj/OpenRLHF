45946900637b1f51c67e6b6a9712132cf3993f0f
cadentj <caden.juang@gmail.com>
2025-03-05 02:06:35 +0000

    Reduce micro batch size for Gemma 2 SFT training

--- train_w_ablations.sh
+++ train_w_ablations.sh
@@ -8,7 +8,7 @@ deepspeed --module openrlhf.cli.train_sft \
     --eval_split val \
     --input_template $'### User:\n{}\n\n### Assistant:\n' \
     --train_batch_size 32 \
-    --micro_train_batch_size 4 \
+    --micro_train_batch_size 2 \
     --max_samples 500000 \
     --pretrain google/gemma-2-9b \
     --save_path /workspace/gemma-ablations \
